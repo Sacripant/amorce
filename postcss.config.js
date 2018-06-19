bb0131406280d9c3cf5a5d319422dedcda9d90ae
Thomas Jund <thomas@sacripant.fr>
2018-06-19 17:48:26 +0200

    npm script: add prod with NODE_ENV=production for postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,30 @@
-module.exports = {
-  map: true,
+// module.exports = {
+//   map: true,
+//   verbose: true,
+//   plugins: [
+//     require('postcss-import')(),
+//     require("postcss-cssnext")({
+//         browsers: ['last 3 versions', 'Firefox ESR', 'Firefox 28']
+//     })
+//     // require('cssnano')()
+//   ]
+// }
+
+// console.log(ctx);
+console.log(process.env.NODE_ENV);
+
+module.exports = ctx => ({
+  map: process.env.NODE_ENV === "production" ? false : true,
   verbose: true,
-  plugins: [
-    require('postcss-import')(),
-    require("postcss-cssnext")({
+  plugins: {
+    'postcss-import': { root: ctx.file.dirname },
+    'postcss-cssnext': {
         browsers: ['last 3 versions', 'Firefox ESR', 'Firefox 28']
-    })
-    // require('cssnano')()
-  ]
-}
+    },
+    cssnano: process.env.NODE_ENV === "production" ? {
+      preset: 'default'
+    } : false
+  }
+});
+
+
